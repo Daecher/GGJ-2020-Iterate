3c803608dd3eff2cc61c1bbb3656bc224a3e4199
Kyle Hooks <dmuntrasi@gmail.com>
2020-02-01 10:31:54 -0600

    Simple player generation is working

--- Assets/Scripts/GenerateIterator.cs
+++ Assets/Scripts/GenerateIterator.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class GenerateIterator : MonoBehaviour
 {
-    public Material templateMaterial;
     public List<GameObject> templateHeads;
     public List<GameObject> templateBodies;
-
-    public int seed = 0;
     public string stringSeed = "";
+    public Transform headLocation;
+    public Transform bodyLocation;
+
+    GameObject generatedHead;
+    GameObject generatedBody;
+    Material generatedMaterial;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,13 +25,7 @@ public class GenerateIterator : MonoBehaviour
     {
         if (Input.GetKeyDown("r"))
         {
-            Debug.Log(stringSeed);
-            stringSeed.GetHashCode();
-            Random.InitState(stringSeed.GetHashCode());
-            Debug.Log(Random.Range(
-                0, 
-                Mathf.FloorToInt(Mathf.Pow(2, 16))
-            ));
+            Generate();
         }
     }
 
@@ -38,8 +36,44 @@ public class GenerateIterator : MonoBehaviour
          *  are generated with random shapes and colors.
          * The player camera should always be able to see the body
          *  (but not the head) as well as their hands/grabbers/whatever.
-         * 
-         * 
          * */
+
+        Debug.Log(stringSeed);
+        stringSeed.GetHashCode();
+        Random.InitState(stringSeed.GetHashCode());
+        Debug.Log(Random.Range(
+            0,
+            Mathf.FloorToInt(Mathf.Pow(2, 16))
+        ));
+
+        // Generate head
+        // Pick head shape
+        // Pick x/y/z scale
+
+        int headChoice = Random.Range(0, templateHeads.Count);
+        Vector3 headScale = new Vector3(
+            Random.Range(0.5f, 1.5f),
+            Random.Range(0.5f, 1.5f),
+            Random.Range(0.5f, 1.5f)
+            );
+        Color colorChoice = Random.ColorHSV();
+
+        var head = Instantiate(templateHeads[headChoice], headLocation.transform.position, Quaternion.identity);
+        head.GetComponent<Renderer>().material.color = colorChoice;
+        head.transform.localScale = headScale;
+
+        // Generate body
+        // Pick body color
+        // Pick x/y/z scale
+
+        int bodyChoice = Random.Range(0, templateBodies.Count);
+        Vector3 bodyScale = new Vector3(
+            Random.Range(0.5f, 1.5f),
+            Random.Range(0.5f, 1.5f),
+            Random.Range(0.5f, 1.5f)
+            );
+        var body = Instantiate(templateBodies[bodyChoice], bodyLocation.position, Quaternion.identity);
+        body.GetComponent<Renderer>().material.color = colorChoice;
+        body.transform.localScale = bodyScale;
     }
 }
